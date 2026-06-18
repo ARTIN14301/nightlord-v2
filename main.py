d71d97cfcb1fc5008f45d7cc0d49e4ff6559a078
ARTIN14301 <artin112raider@gmail.com>
2026-06-18 18:34:20 +0330

    Refactor imports and update welcome message

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from telegram import Update
-from telegram.ext import (
-    Application,
-    CommandHandler,
-    ContextTypes
-)
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 from config import TOKEN
+from database.db import init_db
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "🌑 Night Lord V2 Online"
+        "🌑 Welcome to Night Lord V2"
     )
 
 
 def main():
+    init_db()
+
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
