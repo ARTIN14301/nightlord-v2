fbafc36828fb62a1c426c8f88d412fabe8b5a43e
ARTIN14301 <artin112raider@gmail.com>
2026-06-18 18:43:21 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-
+from handlers.register import register, handle_class
 from config import TOKEN
 from database.db import init_db
-
+from telegram.ext import MessageHandler, filters, CommandHandler
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
@@ -14,6 +14,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 def main():
     init_db()
 
+    app.add_handler(CommandHandler(["register", "ثبت_نام"], register))
+
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_class))
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
